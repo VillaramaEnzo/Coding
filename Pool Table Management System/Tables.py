e008d492dc60cdb3ea1365dc548693aac808a4e4
____e_vil <69020282+VillaramaEnzo@users.noreply.github.com>
2021-06-09 17:56:46 +1200

    Class Creation

--- Pool Table Management System/Tables.py
+++ Pool Table Management System/Tables.py
@@ -1,5 +1,34 @@
+import datetime as dt
+
+
 class Table:
 
     def __init__(self, number):
 
         self.tableNo = number
+        self.inUse = False
+        self.TCPH = 16     # Table Cost Per Hour
+
+    def getTableNo(self):
+
+        return self.tableNo
+
+    def getInUse(self):
+
+        return self.inUse
+
+    def setStartTime(self):
+
+        time = dt.datetime.now().strftime('%Y/%m/%d - %I:%M')
+        print(time)
+
+        return time
+
+    def calcCosts(self, starttime, endtime):
+
+        return
+
+
+table = Table(2)
+
+table.setStartTime()
